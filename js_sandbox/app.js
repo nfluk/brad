f8241e9439c4892c021bcd5b206e37eee68fb38d
nfluk <nickylukic@gmail.com>
2022-06-08 18:38:49 +0200

    trying different console logs

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,2 +1,9 @@
-console.log('Hello World!');
-console.table({ name: 'John', occupation: 'carpenter' });
+//console.log('Hello World!');
+//console.table({ name: 'John', occupation: 'carpenter' });
+
+let value = 5;
+
+console.log(value);
+console.log(typeof value);
+console.log(value.toFixed());
+console.log(typeof value.toFixed());
