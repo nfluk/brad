a01ade1f8497e113318a8e83e576b58a9eff9c05
nfluk <nickylukic@gmail.com>
2022-06-14 16:48:14 +0200

    testing some commont Math methods

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,9 +1,14 @@
-console.log('Hello World!');
-console.table({ name: 'John', occupation: 'carpenter' });
+const num1 = 100;
+const num2 = 50;
 
-let value = 5;
+let val;
 
-console.log(value);
-console.log(typeof value);
-console.log(value.toFixed());
-console.log(typeof value.toFixed());
+// simple math with numbers
+val = Math.round(3.4); // regular rounding
+val = Math.floor(3.6); // rounds down to closest integer
+val = Math.ceil(3.2); // founds up to closest integer
+val = Math.sqrt(25); // gets the square of the number
+val = Math.pow(5, 2); // takes the first num to the power of the second
+val = Math.abs(-5); // gets the absolute number (disregards from negative numbers)
+
+console.log(val);
