ce3b5033f7a6a0830a6d69f77e99876ad8af3640
nfluk <nickylukic@gmail.com>
2022-06-21 22:34:44 +0200

    playing around with some arrays

---
+++ js_sandbox/arrays.js
@@ -0,0 +1,9 @@
+const numbers = [2, 53, 32, 15, 66, 93, 5];
+const numbers2 = new Array(34, 344, 31, 64, 6);
+const fruit = ['Apple', 'Banana', 'Pear'];
+const mixed = [2, 'car', true, undefined, { a: 33, b: 4 }];
+
+let val;
+
+console.log(numbers);
+console.log(val);
