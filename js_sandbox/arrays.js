32f77a199c06a6de48fe6fa0660a11267b2e17d1
nfluk <nickylukic@gmail.com>
2022-06-23 22:56:10 +0200

    added some extra variables

--- js_sandbox/arrays.js
+++ js_sandbox/arrays.js
@@ -2,6 +2,7 @@ const numbers = [2, 53, 32, 15, 66, 93, 5];
 const numbers2 = new Array(34, 344, 31, 64, 6);
 const fruit = ['Apple', 'Banana', 'Pear'];
 const mixed = [2, 'car', true, undefined, { a: 33, b: 4 }];
+const mixed2 = [[33, 34, 35], 2, 'car', true, undefined, { a: 33, b: 4 }];
 
 let val;
 
